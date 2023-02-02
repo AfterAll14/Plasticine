842a86741904c47681902523c6f8f4606dadc526
AfterAll14 <romanmozhar@yandex.ru>
2023-02-02 10:08:31 +0000

    automatic data normalization

--- Data.cs
+++ Data.cs
@@ -9,10 +9,33 @@ namespace Plasticine
         protected real[] input;
         protected real[] correctResults;
 
-        public NeuralNetworkData(real[] input, real[] correctResults)
+        public NeuralNetworkData(real[] input, real[] correctResults, bool normalizeAutomatically = false)
         {
             this.input = input;
             this.correctResults = correctResults;
+
+            if (normalizeAutomatically)
+            {
+                real minValue = input[0];
+                real maxValue = input[0];
+
+                for (int i = 1; i < input.Length; i++)
+                {
+                    if (input[i] < minValue)
+                        minValue = input[i];
+
+                    if (input[i] > maxValue)
+                        maxValue = input[i];
+                }
+
+                real range = maxValue - minValue;
+
+                if (range != (real)0.0)
+                {
+                    for (int i = 0; i < input.Length; i++)
+                        input[i] = (input[i] - minValue) / range;
+                }
+            }
         }
 
         public NeuralNetworkData()
