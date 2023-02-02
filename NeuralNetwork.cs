acd15b4cbbbaad04105c8a2e2e9d5fba0a5a6312
AfterAll14 <romanmozhar@yandex.ru>
2023-02-02 04:52:45 +0000

    nn data non abstract

--- NeuralNetwork.cs
+++ NeuralNetwork.cs
@@ -293,13 +293,56 @@ namespace Plasticine
         }
     }
 
-    public abstract class NeuralNetworkData : CustomSerializable
+    public class NeuralNetworkData : CustomSerializable
     {
-        abstract public real[] GetInput();
+        protected real[] input;
+        protected real[] correctResults;
 
-        abstract public real[] GetCorrectResult();
+        public NeuralNetworkData(real[] input, real[] correctResults)
+        {
+            this.input = input;
+            this.correctResults = correctResults;
+        }
 
-        abstract public real CalculateResultAccuracy(real[] nnOuput);
+        public NeuralNetworkData()
+        {
+
+        }
+
+        public real[] GetInput()
+        {
+            return input;
+        }
+
+        public real[] GetCorrectResult()
+        {
+            return correctResults;
+        }
+
+        virtual public real CalculateResultAccuracy(real[] nnOuput)
+        {
+            int correctPredictions = 0;
+
+            for (int i = 0; i < correctResults.Length; i++)
+            {
+                if (Math.Abs(correctResults[i] - nnOuput[i]) < (real)0.05)
+                    correctPredictions++;
+            }
+
+            return (real)correctPredictions / (real)correctResults.Length;
+        }
+
+        public override void WriteValues(BinaryDataWriter binaryWriter)
+        {
+            binaryWriter.Write(input);
+            binaryWriter.Write(correctResults);
+        }
+
+        public override void ReadValues(BinaryDataReader binaryReader)
+        {
+            input = binaryReader.ReadDoubleArray();
+            correctResults = binaryReader.ReadDoubleArray();
+        }
     }
 
     public class NeuralNetwork : CustomSerializable
